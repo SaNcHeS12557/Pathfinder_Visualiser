b0b6cf6efac9156f9d2b3d58807159901cf15236
SaNcHeS12557 <rubick12557@gmail.com>
2025-05-02 13:45:51 +0200

    AStar Done

--- Pathfinder Visualiser/Pathfinder Visualiser.cpp
+++ Pathfinder Visualiser/Pathfinder Visualiser.cpp
@@ -2,8 +2,23 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/Mouse.hpp>
 #include <vector>
+#include <queue>
+#include <climits>
+#include <unordered_map>
 
 using namespace std;
+using pii = pair<int, int>;
+
+// PIXEL (NODE REPRESENTATION) "A*"
+struct Node {
+	int f, g;
+	int row, col;
+
+	bool operator>(const Node &other) const {
+		return f > other.f;
+	}
+
+};
 
 // PIXEL DEFINITION
 unsigned int pixelHeight = 25; // pixel's height
@@ -21,6 +36,7 @@ bool startChoosed = false;
 sf::Vector2i startPixel{ -1, -1 };
 bool endChoosed = false;
 sf::Vector2i endPixel{ -1, -1 };
+bool gridChanged = false;
 
 bool clickMade = false;
 /*
@@ -63,6 +79,7 @@ void clearAll(vector<vector<int>> &grid) {
 	isGridCleared = true;
 	startChoosed = false;
 	endChoosed = false;
+	gridChanged = true;
 
 	cout << "C Pressed\n";
 }
@@ -70,9 +87,72 @@ void clearAll(vector<vector<int>> &grid) {
 // A* ALGO:
 
 // HEURUSTICS:
-// todo manhattan distance
+int h(int x1, int x2, int y1, int y2) {
+	return abs(x1 - x2) + abs(y1 - y2);
+}
 
-// todo a*
+// clear algo's path
+void clearAlgPath() {
+	for (auto &row : pixelGrid)
+		replace(row.begin(), row.end(), 4, 0);
+}
+
+void runAStar(vector<vector<int>>& grid, sf::Vector2i start, sf::Vector2i goal) {
+	cout << "AStar started!\n";
+	// clear recent algo path
+	clearAlgPath();
+
+	//check board borders
+	auto checkBorders = [&](int r, int c) {
+		return r >= 0 && r < HPixelsCount && c >= 0 && c < WPixelsCount;
+	};
+	
+	// directions
+	auto neighbors = vector<pii>{ {0,1},{1,0},{-1,0},{0,-1} };
+
+	priority_queue<Node, vector<Node>, greater<Node>> openSet;
+	vector<vector<int>> GScore(HPixelsCount, vector<int>(WPixelsCount, INT_MAX));
+	vector<vector<pii>> camePath(HPixelsCount, vector<pii>(WPixelsCount, { -1, -1 }));
+
+	// start
+	GScore[start.x][start.y] = 0;
+	int f = h(start.x, goal.x, start.y, goal.y);
+	openSet.push({ f, 0, start.x, start.y });
+
+	while (!openSet.empty()) {
+		Node curr = openSet.top(); openSet.pop();
+
+		if (curr.row == goal.x && curr.col == goal.y) {
+			int r = goal.x, c = goal.y;
+
+			while (!(r == start.x && c == start.y)) {
+				if (grid[r][c] != 3) grid[r][c] = 4;
+				auto [pr, pc] = camePath[r][c];
+				r = pr; c = pc;
+			}
+
+			return;
+		}
+		for (auto [dr, dc] : neighbors) {
+			int nr = curr.row + dr, nc = curr.col + dc;
+
+			if (!checkBorders(nr, nc)) continue;
+			if (grid[nr][nc] == 1) continue;
+
+			int G = GScore[curr.row][curr.col] + 1;
+			if (G < GScore[nr][nc]) {
+				camePath[nr][nc] = { curr.row, curr.col };
+				GScore[nr][nc] = G;
+				int f = G + h(nr, goal.x, nc, goal.y);
+
+				openSet.push({ f, G, nr, nc });
+			}
+		}
+	}
+
+	cout << "No path!!!\n";
+}
+
 
 int main()
 {
@@ -90,6 +170,11 @@ int main()
 			else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
 				if (keyPressed->scancode == sf::Keyboard::Scancode::C)
 					clearAll(pixelGrid);
+				//else if (keyPressed->scancode == sf::Keyboard::Scancode::A)
+				//	if (startChoosed && endChoosed)
+				//		runAStar(pixelGrid, startPixel, endPixel);
+				//	else
+				//		cout << "Pick start and end first!\n";
 
 		}
 
@@ -124,6 +209,7 @@ int main()
 							pixelGrid[rowIndx][colIndx] = 2;
 							startPixel = { rowIndx, colIndx };
 							startChoosed = true;
+							gridChanged = true;
 						}
 					}
 					else if (!endChoosed) {
@@ -135,6 +221,7 @@ int main()
 							pixelGrid[rowIndx][colIndx] = 3;
 							endPixel = { rowIndx, colIndx };
 							endChoosed = true;
+							gridChanged = true;
 						}
 					}
 					else {
@@ -148,12 +235,16 @@ int main()
 
 				pixelGrid[rowIndx][colIndx] = 1; 
 				isGridCleared = false;
+				gridChanged = true;
+
 			}
 			/*drawScreenPixel(&window, x, y, sf::Color::Black);*/
 			else if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Right)) {
 				checkStartEndRedraw(rowIndx, colIndx);
 
-				pixelGrid[rowIndx][colIndx] = 0; 
+				pixelGrid[rowIndx][colIndx] = 0;
+				gridChanged = true;
+
 			}
 			//drawScreenPixel(&window, x, y, sf::Color::White);
 		}
@@ -169,14 +260,21 @@ int main()
 					drawScreenPixel(&window, x, y, sf::Color::Black);
 				else if (pixelGrid[row][col] == 0)
 					drawScreenPixel(&window, x, y, sf::Color::White);
-				else
+				else if(pixelGrid[row][col] == 2 || pixelGrid[row][col] == 3)
 					drawScreenPixel(&window, x, y, sf::Color::Red);
+				else
+					drawScreenPixel(&window, x, y, sf::Color::Magenta);
 			}
 		}
 
 		// single click processing
 		if (!sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
 			clickMade = false;
+		}
+
+		if (startChoosed && endChoosed && gridChanged) {
+			gridChanged = false;
+			runAStar(pixelGrid, startPixel, endPixel);
 		}
 
 		window.display();
